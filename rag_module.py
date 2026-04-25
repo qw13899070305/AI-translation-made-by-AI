cb068ccfa366e7efcab5ccedae668e906e9e47d6
qw13899070305 <167614360+qw13899070305@users.noreply.github.com>
2026-04-26 04:07:32 +0800

    Add files via upload

--- rag_module.py
+++ rag_module.py
@@ -1,5 +1,5 @@
 import os
-from langchain.document_loaders import PyPDFLoader, TextLoader
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Chroma
